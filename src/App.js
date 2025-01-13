8927e92e836aeead439d1a37a2ed86ef6b0bcd7e
zzeynepcvk <210541009@firat.edu.tr>
2025-01-13 19:09:20 +0300

    pagination - foodlistapi

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(3);
+
+  function showMoreMeals() {
+    setVisible((item) => item += 3);
+  }
+
+  useEffect(() => { fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=british").then((res) => res.json()).then((data) => setItems(data.meals)); }, []);
+  return (
+    <div className="App">
+      <h1 className="header"> British Meals</h1>
+      <div className="container">
+        {items.slice(0,visible).map(item => (
+          <div className='card'>
+            <div className='image'>
+              <img src={item.strMealThumb}  />
+
+            </div>
+            <p>{item.strMeal}</p>
+          </div>
+        ))}
+
+        <button onClick={showMoreMeals}>Click for more meals</button>
+      </div> 
+    </div>
+  );
+}
+
+export default App;
